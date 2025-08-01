05b6c80de67602206617b1eea4660314ebf04c57
Andrew Wang <wthsir@gmail.com>
2025-08-01 20:02:04 +1000

    docs: add comment

--- HelperFunctions/IdGeneratorFactory.cs
+++ HelperFunctions/IdGeneratorFactory.cs
@@ -7,7 +7,10 @@ namespace Domain.SharedKernel.HelperFunctions
         private static IdGenerator? _generator;
         private static readonly object _lock = new();
         private static bool _initialized = false;
-
+        /// <summary>
+        /// in distributed systems, workerId should be unique for each instance
+        /// </summary>
+        /// <param name="workerId"></param>
         public static void Initialize(int workerId)
         {
             lock (_lock)
