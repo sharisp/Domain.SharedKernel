a0386dd3cc92cff8f6e44e08bab22b08898fc855
Andrew Wang <wthsir@gmail.com>
2025-08-12 22:18:07 +1000

    feat: add snowflakeid unittest

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -1,9 +1,52 @@
+using Domain.SharedKernel;
+using Domain.SharedKernel.HelperFunctions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
 namespace UnitTest
 {
     [TestClass]
     public class UnitTest1
     {
-      
-    
+        private ServiceProvider _serviceProvider;
+
+      //  private AuthenticationTokenResponse authenticationTokenResponse;
+        [TestInitialize] // run before each test
+        public void Setup()
+        {
+            var services = new ServiceCollection();
+            services.AddDomainShardKernelCollection(new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build());
+            _serviceProvider = services.BuildServiceProvider();
+           // authenticationTokenResponse = _serviceProvider.GetService<AuthenticationTokenResponse>();
+
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            if (_serviceProvider != null)
+            {
+                _serviceProvider.Dispose();
+            }
+        }
+
+
+        [TestMethod]
+        public void Test1()
+        {
+            IdGeneratorFactory.Initialize(1);
+            var newID=  IdGeneratorFactory.NewId();          
+            Assert.IsTrue(newID > 0, "newID should have value");
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            var newID = IdGeneratorFactory.NewId();
+            Assert.IsTrue(newID > 0, "newID should have value");
+        }
     }
 }
