2114717df8bee5006d443735246039bdffc3dc91
Andrew Wang <wthsir@gmail.com>
2025-08-12 22:02:10 +1000

    refactor: add unit test

---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Common.Jwt;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JwtUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+      
+    
+    }
+}
